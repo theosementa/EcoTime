5722203fa36b45eec1397c02b7169f2e070254dc
Theo Sementa <theosementa@gmail.com>
2024-07-15 13:05:33 +0200

    Initial Commit

---
+++ EcoTime/EcoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoTimeApp.swift
+//  EcoTime
+//
+//  Created by KaayZenn on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct EcoTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
